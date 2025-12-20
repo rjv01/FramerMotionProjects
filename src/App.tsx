af4470b9625b4bdb490973686d87928c81ad9be5
rjv01 <verma.rajshekhar1@gmail.com>
2025-12-21 00:24:44 +0530

    adding router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import Home from "./components/Home"
 import Subscribe from "./components/Subscribe"
-
+import { Routes,Route } from "react-router-dom"
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<Home />}>Home</Route>
+        <Route path="/subscribe" element={<Subscribe />} />
+      </Routes>
       {/* <h1 className="text-3xl font-bold text-red-300">Hello world</h1> */}
-      <Subscribe />
     </div>
   )
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,22 @@
+
+function Home() {
+  return (
+    <div className='min-h-screen flex flex-col items-center bg-neutral-900'>
+        <h1 className='text-4xl text-green-400 mt-8'>
+            Welcome to the Home Page</h1>
+        <div className="font-mono grid grid-cols-3 gap-10 justify-center items-center m-6 text-white text-2xl">
+            <a href="/subscribe" className="text-green-400 bg-green-500/30 hover:text-green-300 transition-colors duration-200 p-4 m-4 text-center rounded-lg">Subscribe Button</a>
+            <a href="/subscribe" className="text-green-400 bg-green-500/30 hover:text-green-300 transition-colors duration-200 p-4 m-4 text-center rounded-lg">Subscribe Button</a>
+            <a href="/subscribe" className="text-green-400 bg-green-500/30 hover:text-green-300 transition-colors duration-200 p-4 m-4 text-center rounded-lg">Subscribe Button</a>
+            <a href="/subscribe" className="text-green-400 bg-green-500/30 hover:text-green-300 transition-colors duration-200 p-4 m-4 text-center rounded-lg">Subscribe Button</a>
+            <a href="/subscribe" className="text-green-400 bg-green-500/30 hover:text-green-300 transition-colors duration-200 p-4 m-4 text-center rounded-lg">Subscribe Button</a>
+            <a href="/subscribe" className="text-green-400 bg-green-500/30 hover:text-green-300 transition-colors duration-200 p-4 m-4 text-center rounded-lg">Subscribe Button</a>
+            <a href="/subscribe" className="text-green-400 bg-green-500/30 hover:text-green-300 transition-colors duration-200 p-4 m-4 text-center rounded-lg">Subscribe Button</a>
+            <a href="/subscribe" className="text-green-400 bg-green-500/30 hover:text-green-300 transition-colors duration-200 p-4 m-4 text-center rounded-lg">Subscribe Button</a>
+            <a href="/subscribe" className="text-green-400 bg-green-500/30 hover:text-green-300 transition-colors duration-200 p-4 m-4 text-center rounded-lg">Subscribe Button</a>
+        </div>
+    </div>
+  )
+}
+
+export default Home
