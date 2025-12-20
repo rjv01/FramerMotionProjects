36aa7f5a198a96029ebe6a65fa7600b84ab3e61d
rjv01 <verma.rajshekhar1@gmail.com>
2025-12-20 23:55:22 +0530

    adding bg-radical

--- src/components/Subscribe.tsx
+++ src/components/Subscribe.tsx
@@ -1,7 +1,14 @@
 
 function Subscribe() {
   return (
-    <div className="min-h-screen flex justify-center items-center bg-neutral-900">
+    <div 
+        className="min-h-screen flex justify-center items-center bg-neutral-900"
+        style={{
+            backgroundImage: `radial-gradient(circle at 0.5px 0.5px, rgba(6,182,212,0.2) 0.5px, transparent 0)`,
+            backgroundSize: `8px 8px`,
+            backgroundRepeat: `repeat`,
+        }}
+    >
       <button
         className="group relative text-neutral-500 px-12 py-4 rounded-lg bg-black shadow-[0px_1px_2px_0px_rgba(255,255,255,0.1)_inset,0px_-1px_2px_0px_rgba(255,255,255,0.1)_inset] cursor-pointer"
         >Subscribe
